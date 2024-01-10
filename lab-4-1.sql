05ac9ed9461fda438470b7bddd56568bc1bbbe12
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-01-10 17:29:12 +0000

    Practice

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,10 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT SUM(stats.hits) 
+from stats
+INNER JOIN players ON stats.player_id = players.id
+WHERE first_name = "Barry" AND last_name = "Bonds"
+
 -- Expected result:
 -- 2935
 
